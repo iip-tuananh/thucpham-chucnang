4cc13a54905472ed4266899c163fd44640ca8ad1
Duongatun19 <95019544+Duongatun19@users.noreply.github.com>
2023-03-17 17:16:06 +0700

    up

--- resources/views/chungnhan.blade.php
+++ resources/views/chungnhan.blade.php
@@ -57,7 +57,7 @@
       
                     <div class="box_chungnhan">
                         <div class="img_chungnhan">
-                            <a data-fancybox="gallery0" href="javascript:;"><img src="{{$item->image}}"></a>
+                            <a  href="javascript:;"><img src="{{$item->image}}"></a>
                         </div>
                     </div>
                 @endforeach
