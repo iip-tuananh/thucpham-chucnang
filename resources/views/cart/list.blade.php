2220391526183f73c5dfb6b789aa16169633960e
Duongatun19 <95019544+Duongatun19@users.noreply.github.com>
2023-03-20 15:41:23 +0700

    up

--- resources/views/cart/list.blade.php
+++ resources/views/cart/list.blade.php
@@ -12,24 +12,27 @@ Giỏ hàng của bạn
 @endsection
 @section('js')
 <script>
-   $('.remove_item_cart').click(function (e) { 
-      e.preventDefault();
-         var id = $(this).data('id');
-         var url = $(this).data('url');
-         $.ajax({
-            type: "get",
-            url: url,
-            data: {
-               id:id,
-            },
-            success: function (data) {
-               $('.listcartajax').html(data.html3);
-               $('.count-item').html(data.html2);
-            }
-         });
-   });
-   
-   function btnMinus(id,url) {
+  $('.remove_item_cart').click(function (e) { 
+       e.preventDefault();
+          var id = $(this).data('id');
+          var url = $(this).data('url');
+          $.ajax({
+             type: "get",
+             url: url,
+             data: {
+                id:id,
+             },
+             success: function (data) {
+                $('.cart-layout-new').html(data.html3);
+                $('.count-item').html(data.html2);
+           
+             }
+          });
+    });
+
+    
+    
+    function btnMinus(id,url) {
     var id = id;
     var result = document.getElementById('qty'+id); var qtypro = result.value; if( !isNaN( qtypro ) && qtypro > 1 ) result.value--;
     var quantity = result.value;
@@ -39,11 +42,12 @@ Giỏ hàng của bạn
         url:url,
         data: {id:id, quantity:quantity},
         success: function(data) {
-         $('.listcartajax').html(data.html3);
+            $('.cart-layout-new').html(data.html3);
+
         }
     })
-   }
-   function btnPlus(id , url) {
+    }
+    function btnPlus(id , url) {
     var id = id;
     console.log(id);
     var result = document.getElementById('qty'+id); var qtypro = result.value; if( !isNaN( qtypro )) result.value++;
@@ -54,10 +58,11 @@ Giỏ hàng của bạn
         url:url,
         data: {id:id, quantity:quantity},
         success: function(data) {
-         $('.listcartajax').html(data.html3);
+            $('.cart-layout-new').html(data.html3);
+     
         }
     })
-   }
+    }
 </script>
 @endsection
 @section('content')
@@ -158,54 +163,6 @@ Giỏ hàng của bạn
    </div>
 </div>
 <script>
-    $('.remove_item_cart').click(function (e) { 
-       e.preventDefault();
-          var id = $(this).data('id');
-          var url = $(this).data('url');
-          $.ajax({
-             type: "get",
-             url: url,
-             data: {
-                id:id,
-             },
-             success: function (data) {
-                $('.cart-layout-new').html(data.html3);
-                $('.count-item').html(data.html2);
-           
-             }
-          });
-    });
-    
-    function btnMinus(id,url) {
-    var id = id;
-    var result = document.getElementById('qty'+id); var qtypro = result.value; if( !isNaN( qtypro ) && qtypro > 1 ) result.value--;
-    var quantity = result.value;
-    var url = url;
-    $.ajax({
-        type:'get',
-        url:url,
-        data: {id:id, quantity:quantity},
-        success: function(data) {
-            $('.cart-layout-new').html(data.html3);
-
-        }
-    })
-    }
-    function btnPlus(id , url) {
-    var id = id;
-    console.log(id);
-    var result = document.getElementById('qty'+id); var qtypro = result.value; if( !isNaN( qtypro )) result.value++;
-    var quantity = result.value;
-    var url = url;
-    $.ajax({
-        type:'get',
-        url:url,
-        data: {id:id, quantity:quantity},
-        success: function(data) {
-            $('.cart-layout-new').html(data.html3);
-     
-        }
-    })
-    }
+  
  </script>
 @endsection
