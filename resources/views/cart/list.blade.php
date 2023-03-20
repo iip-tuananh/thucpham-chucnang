942f1674e610106b7636015bae3982ecb0cfa3c7
Duongatun19 <95019544+Duongatun19@users.noreply.github.com>
2023-03-20 13:13:18 +0700

    up

--- resources/views/cart/list.blade.php
+++ resources/views/cart/list.blade.php
@@ -5,83 +5,150 @@ Giỏ hàng của bạn
 @section('description')
 @endsection
 @section('image')
+{{url(''.$banners[0]->image)}}
+@endsection
+@section('css')
+<link rel="stylesheet" href="{{asset('frontend/css/cart_style.scss.css')}}">
+@endsection
+@section('js')
+<script>
+   $('.remove_item_cart').click(function (e) { 
+      e.preventDefault();
+         var id = $(this).data('id');
+         var url = $(this).data('url');
+         $.ajax({
+            type: "get",
+            url: url,
+            data: {
+               id:id,
+            },
+            success: function (data) {
+               $('.listcartajax').html(data.html3);
+               $('.count-item').html(data.html2);
+            }
+         });
+   });
+   
+   function btnMinus(id,url) {
+    var id = id;
+    var result = document.getElementById('qty'+id); var qtypro = result.value; if( !isNaN( qtypro ) && qtypro > 1 ) result.value--;
+    var quantity = result.value;
+    var url = url;
+    $.ajax({
+        type:'get',
+        url:url,
+        data: {id:id, quantity:quantity},
+        success: function(data) {
+         $('.listcartajax').html(data.html3);
+        }
+    })
+   }
+   function btnPlus(id , url) {
+    var id = id;
+    console.log(id);
+    var result = document.getElementById('qty'+id); var qtypro = result.value; if( !isNaN( qtypro )) result.value++;
+    var quantity = result.value;
+    var url = url;
+    $.ajax({
+        type:'get',
+        url:url,
+        data: {id:id, quantity:quantity},
+        success: function(data) {
+         $('.listcartajax').html(data.html3);
+        }
+    })
+   }
+</script>
 @endsection
 @section('content')
-<div class="breadcrumb-area">
-   <div class="container">
-       <div class="row">
-           <div class="col-md-12">
-               <div class="breadcrumb-content">
-                   <ul class="nav">
-                       <li><a href="{{route('home')}}">Trang chủ</a></li>
-                       <li>Giỏ hàng</li>
-                   </ul>
+<div class="col-right position-relative">
+   <div class="contentWarp ">
+      <div class="breadcrumbs bg-white">
+         <div class="container position-relative">
+            <ul class="breadcrumb align-items-center m-0 pl-0 pr-0 small pt-2 pb-2">
+               <li class="home">
+                  <a href="{{route('home')}}" title="Trang chủ">
+                     <svg width="12" height="10.633">
+                        <use href="#svg-home"></use>
+                     </svg>
+                     Trang chủ
+                  </a>
+                  <span class="slash-divider ml-2 mr-2">/</span>
+               </li>
+               <li>Giỏ hàng</li>
+            </ul>
+         </div>
+      </div>
+      <section class="cart-layout mt-3 mb-3">
+         <div class="container" style="min-height: 350px">
+            <div class="rounded p-2 p-md-3 bg-white">
+               <h1 class="cart-name font-weight-bold text-uppercase pb-2 pt-2 mb-2">
+                  Giỏ hàng
+               </h1>
+               @php
+               $totalPrice = 0;
+               @endphp
+               @if(count($cart)>0)
+               <div class="row js-cart listcartajax">
+                  <div class="basket cart__basket col-md-8 ">
+                     @foreach ($cart as $item)
+                     @php
+                     $pricePro = $item['price'] - $item['price'] * ($item['discount']/100);
+                     $totalPrice += $item['quantity'] * $pricePro ;
+                     @endphp
+                     <div class="d-flex cart__basket__item product mb-4 rounded ux-card position-relative clearfix">
+                        <img src="{{$item['image']}}" class="js-img position-absolute" alt="undefined">
+                        <div class="col-12 d-flex p-0">
+                           <p class="item-title clearfix mb-2">
+                              <a href="/xiaomi-redmi-note-11-pro-plus-5g" title="{{languageName($item['name'])}}" class="js-titlte font-weight-bold">{{languageName($item['name'])}}</a>
+                           </p>
+                           <span class="js-price  ml-auto text-right clearfix"><del style="color:black">{{number_format($item['price'])}}</del>&nbsp;₫</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                           <span class="js-price price font-weight-bold  clearfix">{{number_format($pricePro)}}&nbsp;₫</span>
+                        </div>
+                        <div class="input-group-btn " style="display:flex">
+                           <button
+                           onclick="btnMinus('{{$item['id']}}','{{route('updateCart')}}')"
+                           class="reduced_pop items-count btn-minus btn btn-default bootstrap-touchspin-down"
+                           type="button">–</button>
+                           <input
+                              type="text"
+                              maxlength="12" min="1" disabled=""
+                              class="form-control quantity-r2 quantity js-quantity-product input-text number-sidebar input_pop input_pop qtyItem27698914"
+                              id="qty{{$item['id']}}" name="Lines"
+                              size="4" value="{{$item['quantity']}}">
+                              <button
+                              onclick="btnPlus('{{$item['id']}}','{{route('updateCart')}}')"
+                              class="increase_pop items-count btn-plus btn btn-default bootstrap-touchspin-up"
+                              type="button">+</button>
+                           
+                        </div>
+                        <button data-url="{{route('removeCart')}}" data-id="{{$item['id']}}" class="btn btn-outline-danger remove ml-auto remove_item_cart" title="Xoá" data-variantid="70103789">Xoá</button>
+                     </div>
+                     @endforeach
+                  </div>
+                  <div class="summary cart__summary col-md-4">
+                     <dl class="total mb-4 p-3 d-flex align-items-center clearfix flex-wrap justify-content-end rounded">
+                        <dt class="text-uppercase font-weight-bold roun" style="color: black">Tổng tiền</dt>
+                        <dd class="cart__summary_total font-weight-bold ml-auto mb-0">{{number_format($totalPrice)}}&nbsp;₫</dd>
+                     </dl>
+                     <a class="btn btn-block btn-checkout btn-danger rounded mb-3 text-uppercase font-weight-bold pt-3 pb-3" href="{{route('checkout')}}" role="button">Thanh toán</a>
+                     <hr>
+                     <a class="btn btn-block btn-clearcart js-clearcart btn-dark rounded w-100 font-weight-bold mb-4" href="{{route('home')}}" role="button" >Tiếp tục mua hàng</a>
+                  </div>
                </div>
-           </div>
-       </div>
-   </div>
-</div>
-<div class="cart-main-area mtb-60px">
-   <div class="container">
-       <h3 class="cart-page-title">Giỏ hàng của bạn</h3>
-       <div class="row">
-           <div class="col-lg-8 col-md-8 col-sm-12 col-12">
-               <form action="#">
-                   <div class="table-content table-responsive cart-table-content">
-                       <table>
-                           <thead>
-                               <tr>
-                                   <th>Ảnh</th>
-                                   <th>Thông tin sản phẩm</th>
-                                   <th>Đơn giá</th>
-                                   <th>Số lượng</th>
-                                   <th>Thành tiền</th>
-                                   <th>Hành động</th>
-                               </tr>
-                           </thead>
-                           @php $total = 0; $qty = 0 ; @endphp
-                           @foreach((array) session('cart') as $id => $details)
-                                    @php 
-                                    $total += ($details['price'] - ($details['price']*($details['discount']/100))) * $details['quantity'] ;
-                                    $qty += $details['quantity'] ;
-                                    @endphp
-                           @endforeach
-                           <tbody class="cartlist">
-                              @foreach ($cart as $id => $details)
-                               <tr>
-                                   <td class="product-thumbnail">
-                                       <a href="javascript:;"><img class="img-responsive" src="{{ url(''.$details['image']) }}" alt=""></a>
-                                   </td>
-                                   <td class="product-name"><a href="">{{ languageName($details['name']) }}</a></td>
-                                   <td class="product-price-cart"><span class="amount">{{ number_format($details['price'] - ($details['price']*($details['discount']/100))) }}₫</span></td>
-                                   <td class="product-quantity">
-                                       <div class="cart-plus-minus">
-                                          <div class="dec qtybutton" onclick="qtyminus({{$details['id']}})">-</div>
-                                          <input class="cart-plus-minus-box" id="quantity-{{$details['id']}}" type="text" name="qtybutton" value="{{$details['quantity']}}">
-                                          <div class="inc qtybutton" onclick="qtyplus({{$details['id']}})">+</div>
-                                       </div>
-                                   </td>
-                                   <td class="product-subtotal cartprice-{{$id}}">{{ number_format(($details['price'] - ($details['price']*($details['discount']/100)))*$details['quantity']) }}₫</td>
-                                   <td class="product-remove">
-                                       <a href="javascript:;" onclick="removeCart({{$details['id']}})"><i class="icon-close"></i></a>
-                                   </td>
-                               </tr>
-                               @endforeach
-                           </tbody>
-                       </table>
-                   </div>
-               </form>
-           </div>
-           <div class="col-lg-4 col-md-12 mt-md-30px">
-            <div class="grand-totall">
-                <div class="title-wrap">
-                    <h4 class="cart-bottom-title section-bg-gary-cart">Tổng</h4>
-                </div>
-                <h5>Tổng tiền <span class="total-price">{{number_format($total)}}₫</span></h5>
-                <a href="javascript:;" onclick="location.href='/thanh-toan.html'">Thanh toán</a>
+               @else
+               <div class="basket cart__basket col-md-12 ">
+                  <div class="d-flex cart__basket__item product mb-4 rounded ux-card position-relative clearfix">
+                     Hiện tại không có sản phẩm nào trong giỏ hàng
+                  </div>
+               </div>
+               <div class="summary cart__summary col-md-12">
+                  <a class="btn btn-block btn-clearcart js-clearcart btn-dark rounded w-100 font-weight-bold mb-4" href="{{route('home')}}" role="button" >Tiếp tục mua hàng</a>
+               </div>
+               @endif
             </div>
-        </div>
-       </div>
+         </div>
+      </section>
    </div>
 </div>
 @endsection
