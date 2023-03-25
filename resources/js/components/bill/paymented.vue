a623597a066df4e9e1fe5f202ce2b91698d1b15e
Duongatun19 <95019544+Duongatun19@users.noreply.github.com>
2023-03-25 17:13:44 +0700

    up

--- resources/js/components/bill/paymented.vue
+++ resources/js/components/bill/paymented.vue
@@ -35,14 +35,23 @@
               <vs-table max-items="5" pagination :data="list" v-if="list.length > 0">
                 <template slot="thead">
                   <vs-th>Đơn hàng đã hoàn tất</vs-th>
-                  <vs-th>Chỉnh sửa lúc</vs-th>
-                  <vs-th>Khach hàng</vs-th>
+                  <vs-th>Sản phẩm</vs-th>
+                  <vs-th>Giá đơn hàng</vs-th>
+                  <vs-th>Cập nhật lúc</vs-th>
+                  <vs-th>Khách hàng</vs-th>
                   <vs-th>Trạng thái</vs-th>
                   <vs-th>Hành động</vs-th>
                 </template>
                 <template slot-scope="{data}">
                   <vs-tr :key="indextr" v-for="(tr, indextr) in data">
                     <vs-td :data="tr.id">#{{tr.code_bill}}</vs-td>
+                    <vs-td :data="tr.bill_detail">
+                      <div v-for="pr, indexpr in tr.bill_detail" :key="indexpr">
+                        <span :data="pr.id">{{pr.name}}</span><br>
+                      </div>
+                    </vs-td>
+                    <vs-td :data="tr.total_money" v-if="tr.total_money != null">{{formatNumber(tr.total_money)}}₫</vs-td>
+                    <vs-td :data="tr.total_money" v-if="tr.total_money == null">0₫</vs-td>
                     <vs-td :data="tr.updated_at">{{formatDate(tr.updated_at)}}</vs-td>
                     <vs-td :data="tr.cus_name">
                       {{tr.cus_name}}
@@ -61,7 +70,7 @@
                           icon="edit"
                         ></vs-button>
                       </router-link>
-                      <vs-button vs-type="gradient" size="lagre" color="red" icon="delete_forever" @click="confirmDestroy(tr.id)"></vs-button>
+                      <vs-button vs-type="gradient" size="lagre" color="red" icon="delete_forever" @click="confirmDestroy(tr.code_bill)"></vs-button>
                     </vs-td>
                   </vs-tr>
                 </template>
@@ -96,16 +105,19 @@ export default {
     
   },
   methods: {
-    ...mapActions(["draftBill","destroyCate", "loadings"]),
+    ...mapActions(["draftBill","deleteBill", "loadings"]),
     closePop(event) {
       this.listCategory();
       this.popupActivo = event;
     },
     formatDate(value) {
         const date = new Date(value),
-          month = date.getMonth() > 9 ? date.getMonth() : `0${date.getMonth()}`,
+          month = date.getMonth() > 9 ? date.getMonth()+1 : `0${date.getMonth()+1}`,
           day = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`;
-        return `${date.getFullYear()}-${month}-${day}`;
+        return `${day}-${month}-${date.getFullYear()}`;
+    },
+    formatNumber(num) {
+      return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     },
     draftBills() {
       this.loadings(true);
@@ -127,30 +139,30 @@ export default {
           });
       }, 800);
     },
-    // destroy(){
-    //   this.loadings(true);
-    //   this.destroyCate(this.id_item)
-    //   .then(response => {
-    //     this.listCategory()
-    //     this.loadings(false);
-    //     this.$vs.notify({
-    //           title: "Xóa danh mục",
-    //           text: "Thành công",
-    //           color: "success",
-    //           position: "top-right"
-    //         });
-    //   });
-    // },
-    // confirmDestroy(id){
-    //   this.id_item = id;
-    //   this.$vs.dialog({
-    //     type:'confirm',
-    //     color: 'danger',
-    //     title: `Bạn có chắc chắn`,
-    //     text: 'Xóa danh mục này',
-    //     accept:this.destroy
-    //   })
-    // }
+    destroy(){
+      this.loadings(true);
+      this.deleteBill(this.id_item)
+      .then(response => {
+        this.draftBills()
+        this.loadings(false);
+        this.$vs.notify({
+              title: "Xóa danh mục",
+              text: "Thành công",
+              color: "success",
+              position: "top-right"
+            });
+      });
+    },
+    confirmDestroy(id){
+      this.id_item = id;
+      this.$vs.dialog({
+        type:'confirm',
+        color: 'danger',
+        title: `Bạn có chắc chắn`,
+        text: 'Xóa danh mục này',
+        accept:this.destroy
+      })
+    }
   },
   mounted() {
     this.draftBills()
