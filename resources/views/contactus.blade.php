a0ce3b5e9f392924399a4ab1dba6f7a8a2598083
Duongatun19 <95019544+Duongatun19@users.noreply.github.com>
2023-03-28 13:32:49 +0700

    up

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -13,163 +13,107 @@
 @section('js')
 @endsection
 @section('content')
-<main id="page-content">
-	<nav class="site-breadcrumb  bg-contact" aria-label="breadcrumb" style="background-image: url({{url('frontend/images/bg-breadcrumb.jpg')}})">
-		<div class="container">
-			<h2 class="text-uppercase">Liên hệ<span class="text-truncate"> chúng tôi</span></h2>
-		</div>
-	</nav>
-	<section class="box-page contact-page">
-		<div class="container">
-			<div class="contact-head">
-				<div class="contact-title"> Cảm ơn bạn! <br> Đã quan tâm đến {{$setting->company}} <br></div>
-				<div class="desc"> Chúng tôi là đơn vị đi đầu lĩnh vực Xuất nhập khẩu, cung cấp dịch vụ Nhập
-					khẩu chính ngạch hàng hoá từ Trung Quốc về Việt Nam.</div>
-			</div>
-			<div class="contact-body">
-				<div class="row">
-					<div class="col-12 col-md-12 col-lg-6">
-						<div class="left">
-							<div class="box-img"> <noscript>
-								<img data-src="{{asset('frontend/images/contact-img.png')}}" 
-									class="img-w100 lazyload" src="{{asset('frontend/images/contact-img.png')}}">
-									<noscript><img src="{{asset('frontend/images/contact-img.png')}}" class="img-w100">
-									</noscript><img src="{{asset('frontend/images/contact-img.png')}}" 
-									data-src="{{asset('frontend/images/contact-img.png')}}" class="img-w100 lazyloaded"></div>
-							<div class="box-body">
-								<div class="contact-form-title">Liên hệ với {{$setting->company}}</div>
-								<div class="desc text-uppercase">{{$setting->webname}}</div>
-								<ul class="mb-3">
-									<h4 class="contact-page_title">Tại Hà Nội</h4>
-									@foreach (json_decode($setting->address1) as $item)
-										@if ($item->address != "")
-										<li> <span class="box-icon"> <i class="fa fa-map-marked-alt"></i>
-										</span> <span>Văn phòng : {{$item->address}}</span></li>
-										@endif
-									@endforeach
-									<li> <span class="box-icon"> <i class="fa fa-phone-alt"></i> </span>
-										<span>Hotline : <a href="tel:{{$setting->phone1}}">{{$setting->phone1}}</a> 
-											@if ($setting->phone2)
-											<br> Hotline 2 : <a href="tel:{{$setting->phone2}}">{{$setting->phone2}}</a><br>
-											@endif
-										</span>
-									</li>
-									<li> <span class="box-icon"> <i class="fa fa-envelope"></i> </span>
-										<span>{{$setting->email}}</span>
-									</li>
-									<li> <span class="box-icon"> <i class="fa fa-clock"></i> </span>
-										<span>Giờ Làm Việc: 8:00-12:00 &amp; 13:30-17:00 Thứ 2 - Thứ
-											7</span>
-									</li>
-								</ul>
-								<ul>
-									<h4 class="contact-page_title">Tại Bẳng Tường, Trung Quốc</h4>
-									@foreach (json_decode($setting->address3) as $item)
-										@if ($item->name != "")
-											<p class="mb-2 mb-md-3">
-												{{$item->name}}
-											</p>
-										@endif
-										@if ($item->name_code != "")
-											<p class="mb-2 mb-md-3">
-												收货名： {{$item->name_code}}
-											</p>
-										@endif
-										@if ($item->phone_number != "")
-											<p class="mb-2 mb-md-3">
-												电话 : <a href="tel:{{$item->phone_number}}"> {{$item->phone_number}} </a>
-											</p>
-										@endif
-										@if ($item->address != "")
-											<p class="mb-2 mb-md-3">
-												收货地址：{{$item->address}}
-											</p>
-										@endif
-										@if ($item->note != "")
-											<p class="mb-2 mb-md-3">
-												请标注外箱大写: {{$item->note}}
-											</p>
-										@endif
-									@endforeach
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div class="col-12 col-md-12 col-lg-6 mt-4 mt-lg-0 contact-group-order">
-						<div class="right order-second-show mb-5">
-							<div class="contact-form">
-								<div class="contact-form-title">Gửi tin nhắn đến chúng tôi</div>
-								<div role="form" class="wpcf7" id="wpcf7-f8499-o1" lang="vi" dir="ltr">
-									<div class="screen-reader-response">
-										<p role="status" aria-live="polite" aria-atomic="true"></p>
-										<ul></ul>
-									</div>
-									<form action="{{route('postcontact')}}" method="POST" class="wpcf7-form init" novalidate="novalidate" data-status="init">
-										@csrf
-										<div class="form-group">
-											<span class="wpcf7-form-control-wrap your-name">
-												<input type="text" name="name" value="" size="40" class="control-inputs wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control" aria-required="true" aria-invalid="false" placeholder="Nhập họ tên *">
-											</span>
-										</div>
-										<div class="form-group">
-											<span class="wpcf7-form-control-wrap your-phone">
-												<input type="tel" required name="phone" value="" class="control-inputs wpcf7-form-control wpcf7-number wpcf7-validates-as-required wpcf7-validates-as-number form-control" aria-required="true" aria-invalid="false" placeholder="Số điện thoại *">
-											</span>
-										</div>
-										<div class="form-group">
-											<span class="wpcf7-form-control-wrap your-email">
-												<input type="text" name="email" value="" size="40" class="control-inputs wpcf7-form-control wpcf7-text form-control" aria-invalid="false" placeholder="Email">
-											</span>
-										</div>
-										<div class="form-group">
-											<span class="wpcf7-form-control-wrap your-note">
-												<textarea name="mess" cols="40" rows="4" class="wpcf7-form-control wpcf7-textarea form-control control-inputs" aria-invalid="false" placeholder="Nội dung"></textarea>
-											</span>
-										</div>
-										<div class="box-button text-center">
-											<input disabled id="contact-btn" type="submit" value="Gửi tin nhắn" class="wpcf7-form-control has-spinner wpcf7-submit btn btn-gradient">
-										</div>
-									</form>
-								</div>
-								<div class="contact-map mt-3">
-									{!!$setting->iframe_map!!}
-								</div>
-							</div>
-						</div>
-						<div class="order-first-show">
-							<h4 class="contact-page_title">Tại Quảng Châu, Trung Quốc</h4>
-							@foreach (json_decode($setting->address2) as $item)
-								@if ($item->name != "")
-									<p class="mb-2 mb-md-3">
-										{{$item->name}}
-									</p>
-								@endif
-								@if ($item->name_code != "")
-									<p class="mb-2 mb-md-3">
-										收货名： {{$item->name_code}}
-									</p>
-								@endif
-								@if ($item->phone_number != "")
-									<p class="mb-2 mb-md-3">
-										电话 : <a href="tel:{{$item->phone_number}}"> {{$item->phone_number}} </a>
-									</p>
-								@endif
-								@if ($item->address != "")
-									<p class="mb-2 mb-md-3">
-										收货地址：{{$item->address}}
-									</p>
-								@endif
-								@if ($item->note != "")
-									<p class="mb-2 mb-md-3">
-										请标注外箱大写: {{$item->note}}
-									</p>
-								@endif
-							@endforeach
-						</div>
-					</div>
+<div id="content" class="site-content">
+	<div class="page-contact">
+	   <main>
+		  <section class="bannerabout w-100" style="background: url(https://vlive-international.vn/wp-content/themes/v4/assets/images/bannertranglienhe-16401454189.png) no-repeat center; ">
+			 <div class="box_desc_banner">
+				<div class="container">
+				   <div class="box_desc_banner1 active">
+					  <h1>
+						 <p style="    color: #bd221e;"> LIÊN HỆ </p>
+					  </h1>
+				   </div>
 				</div>
-			</div>
-		</div>
-	</section>
-</main>
+			 </div>
+		  </section>
+		  <section class="nav-bredcrum">
+			 <div class="container">
+				<div class=" w-100 ngaytao-bv "> 
+				   <a href="https://vlive-international.vn/vi/" title="Trang chủ"> Trang chủ </a>  / <span>Liên hệ</span> 
+				</div>
+			 </div>
+		  </section>
+		  <section class="section_lienhe">
+			 <div class="container">
+				<div class="title_lienhe  w-100">
+				   <h2>Liên hệ với chúng tôi</h2>
+				</div>
+				<div class="khunglienhe w-100" style="margin-top: 20px;">
+				   <div class="row">
+					  <div class="col-md-6 col-xs-12 noidunglienhe">
+						 <h3 class="uppercase" style="box-sizing: border-box; color: rgb(51, 51, 51); width: 640.825px; margin-top: 0px; margin-bottom: 0.5em; text-rendering: optimizespeed; font-size: 1.25em; font-family: Roboto, sans-serif; line-height: 1.2; letter-spacing: 0.05em; text-transform: uppercase; background-color: rgb(255, 255, 255);"><strong><span style="color:#bd221e;">CÔNG TY TNHH WISE CORRIDOR VIỆT NAM</span></strong></h3>
+						 <p style="box-sizing: border-box; margin-bottom: 1.3em; margin-top: 0px; text-align: justify; color: rgb(51, 51, 51); font-family: Roboto, sans-serif; font-size: 16px; background-color: rgb(255, 255, 255);">Mã số doanh nghiệp: 03113814670 do Sở Kế hoạch &amp; Đầu tư TP Hồ Chí Minh cấp lần đầu ngày 18/05/2016</p>
+						 <h3 class="uppercase" style="box-sizing: border-box; color: rgb(51, 51, 51); width: 640.825px; margin-top: 0px; margin-bottom: 0.5em; text-rendering: optimizespeed; font-size: 1.25em; font-family: Roboto, sans-serif; line-height: 1.2; letter-spacing: 0.05em; text-transform: uppercase; background-color: rgb(255, 255, 255);"><strong><span style="color:#bd221e;">TRỤ SỞ CHÍNH</span></strong></h3>
+						 <p style="box-sizing: border-box; margin-bottom: 1.3em; margin-top: 0px; text-align: justify; color: rgb(51, 51, 51); font-family: Roboto, sans-serif; font-size: 16px; background-color: rgb(255, 255, 255);">Tầng 11 tòa nhà Agrex Tower, 58 Võ Văn Tần, Phường Võ Thị Sáu, Quận 3, TPHCM</p>
+						 <div class="box_hotline_contact">
+							<p>Hotline: <span>0839 69 88 69</span></p>
+							<p>Email: <span>cskh.vlive@gmail.com</span></p>
+							<p>Website: <span>vlive-international.vn</span></p>
+						 </div>
+					  </div>
+					  <div class="col-md-6 col-xs-12 formlienhe">
+						 <div role="form" class="wpcf7" id="wpcf7-f2637-o1" lang="vi" dir="ltr">
+							<div class="screen-reader-response">
+							   <p role="status" aria-live="polite" aria-atomic="true"></p>
+							   <ul></ul>
+							</div>
+							<form action="/vi/lien-he/#wpcf7-f2637-o1" method="post" class="wpcf7-form init" novalidate="novalidate" data-status="init">
+							   <div style="display: none;">
+								  <input type="hidden" name="_wpcf7" value="2637" />
+								  <input type="hidden" name="_wpcf7_version" value="5.5.6" />
+								  <input type="hidden" name="_wpcf7_locale" value="vi" />
+								  <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f2637-o1" />
+								  <input type="hidden" name="_wpcf7_container_post" value="0" />
+								  <input type="hidden" name="_wpcf7_posted_data_hash" value="" />
+							   </div>
+							   <div class="input_lienhe">
+								  <span class="wpcf7-form-control-wrap text-902"><input type="text" name="text-902" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Tên *" /></span>
+							   </div>
+							   <div class="input_lienhe">
+								  <span class="wpcf7-form-control-wrap text-525"><input type="text" name="text-525" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Email *" /></span>
+							   </div>
+							   <div class="input_lienhe">
+								  <span class="wpcf7-form-control-wrap text-663"><input type="text" name="text-663" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Điện thoại *" /></span>
+							   </div>
+							   <div class="input_lienhe">
+								  <span class="wpcf7-form-control-wrap text-848"><input type="text" name="text-848" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Địa chỉ *" /></span>
+							   </div>
+							   <div class="input_lienhe">
+								  <span class="wpcf7-form-control-wrap tieude"><input type="text" name="tieude" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Tiêu đề *" /></span>
+							   </div>
+							   <div class="input_lienhe">
+								  <p>                                <span class="wpcf7-form-control-wrap noidung"><textarea name="noidung" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea" aria-invalid="false" placeholder="Nội dung"></textarea></span></p>
+							   </div>
+							   <div class="input_lienhe input_lienhe1">
+								  <input type="submit" value="Gửi đi" class="wpcf7-form-control has-spinner wpcf7-submit" />
+							   </div>
+							   <p style="display: none !important;">
+								  <label>&#916;<textarea name="_wpcf7_ak_hp_textarea" cols="45" rows="8" maxlength="100"></textarea></label><input type="hidden" id="ak_js_1" name="_wpcf7_ak_js" value="15"/><script>document.getElementById( "ak_js_1" ).setAttribute( "value", ( new Date() ).getTime() );</script>
+							   </p>
+							   <div class="wpcf7-response-output" aria-hidden="true"></div>
+							</form>
+						 </div>
+					  </div>
+				   </div>
+				</div>
+			 </div>
+		  </section>
+		  <section class="section_map">
+			 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.437245878514!2d106.6888895141164!3d10.777784862117395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752fc3631bb125%3A0x2c57b065dcfed645!2sV%20Live%20International!5e0!3m2!1svi!2s!4v1640279814710!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+		  </section>
+		  <script type="text/javascript">
+			 $('#frmcontact input[type=text]')
+			   .on('invalid', function(){
+				 return this.setCustomValidity('Vui lòng điền trường này');
+			   })
+			   .on('input', function(){
+				 return this.setCustomValidity('');
+			   });
+			   
+		  </script>                    
+	   </main>
+	</div>
+ </div>
+ <!-- #content -->
 @endsection
