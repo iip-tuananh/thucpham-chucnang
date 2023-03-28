b313ce04c0cc449fcf2e176d4ec3a5a14806bb59
Duongatun19 <95019544+Duongatun19@users.noreply.github.com>
2023-03-28 13:51:48 +0700

    up

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -107,7 +107,7 @@
 			 </div>
 		  </section>
 		  <section class="section_map">
-			 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.437245878514!2d106.6888895141164!3d10.777784862117395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752fc3631bb125%3A0x2c57b065dcfed645!2sV%20Live%20International!5e0!3m2!1svi!2s!4v1640279814710!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+			{!!$setting->iframe_map!!}
 		  </section>
 		  <script type="text/javascript">
 			 $('#frmcontact input[type=text]')
