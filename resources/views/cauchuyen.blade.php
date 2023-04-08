856b39e3eb99cf2995fcac26c6b648858b544ede
Duongatun19 <95019544+Duongatun19@users.noreply.github.com>
2023-04-08 18:26:15 +0700

    up

--- resources/views/cauchuyen.blade.php
+++ resources/views/cauchuyen.blade.php
@@ -55,10 +55,12 @@ Câu chuyện thương hiệu
                   <div class="col-md-8 col col-xs-12">
                      <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff" class="swiper mySwiper2">
                         <div class="swiper-wrapper">
-                           @dd($cauchuyen);
-                           @foreach ($collection as $item)
+                           @php
+                               $imgs =json_decode($cautruyen->images);
+                           @endphp
+                           @foreach ($imgs as $img)
                               <div class="swiper-slide">
-                              <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                              <img src="{{$img}}" />
                               </div>
                            @endforeach
                           
@@ -69,36 +71,11 @@ Câu chuyện thương hiệu
                       </div>
                       <div thumbsSlider="" class="swiper mySwiper">
                         <div class="swiper-wrapper">
-                          <div class="swiper-slide">
-                            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-                          </div>
-                          <div class="swiper-slide">
-                            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-                          </div>
-                          <div class="swiper-slide">
-                            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-                          </div>
-                          <div class="swiper-slide">
-                            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-                          </div>
-                          <div class="swiper-slide">
-                            <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-                          </div>
-                          <div class="swiper-slide">
-                            <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-                          </div>
-                          <div class="swiper-slide">
-                            <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-                          </div>
-                          <div class="swiper-slide">
-                            <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-                          </div>
-                          <div class="swiper-slide">
-                            <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-                          </div>
-                          <div class="swiper-slide">
-                            <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
-                          </div>
+                           @foreach ($imgs as $img)
+                           <div class="swiper-slide">
+                           <img src="{{$img}}" />
+                           </div>
+                        @endforeach
                         </div>
                       </div>
                   </div>
